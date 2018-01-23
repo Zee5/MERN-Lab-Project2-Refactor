92d3ad4263a60ee0ce0765c385776feb21002ebf
Zelalem Tolera <tolerazelalem@gmail.com>
2018-01-23 14:49:39 -0500

    add error handling to fetch request

--- skydives_react_app/src/SkydiveList.js
+++ skydives_react_app/src/SkydiveList.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react'
+const APIURL = '/api/skydives'
 
 class SkydiveList extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            skydives:[]
+  constructor (props) {
+    super(props)
+    this.state = {
+      skydives: []
+    }
+  }
+
+    // make request to my back end api and fetch data
+  componentDidMount () {
+    this.loadSkydives
+  }
+  // GROP ALL API CALLS UNDER this function
+  // https://github.com/github/fetch/issues/203 -- SOURCE for error handling
+  loadSkydives () {
+    fetch(APIURL) // fetch data from local api
+    .then(resp => {
+      if (!resp.ok) {
+        if (resp.status >= 400 && resp.status < 500) {
+          return resp.json().then(data => {
+            let err = {errorMessage: data.message}
+            throw err
+          })
+        } else {
+          let err = {errorMessage: 'The server is down, Please try again later'}
+          throw err
         }
-    }
-
-
-    componentDidMount(){
-        fetch('/api/skydives')
-        .then(data => data.json())
-        .then(skydives => this.setState({skydives}))
-
-    }
-
-   render () {
+      }
+      return resp.json()
+    })
+  .then(skydives => this.setState({skydives}))
+  }
+  render () {
     return (
-        <div>
-            <h1>Book Your Skydiving!</h1>
-          </div>
-      )
+      <div>
+        <h1>Book Your Skydiving!</h1>
+      </div>
+    )
   }
 }
 export default SkydiveList
