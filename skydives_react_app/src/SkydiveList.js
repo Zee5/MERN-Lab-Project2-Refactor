bc8ea6760be879f5cb11fe9c39141164bea76b39
Zelalem Tolera <tolerazelalem@gmail.com>
2018-01-23 13:55:18 -0500

    fetch data from my backend server

--- skydives_react_app/src/SkydiveList.js
+++ skydives_react_app/src/SkydiveList.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
 
 class SkydiveList extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            skydives:[]
+        }
+    }
+
+
+    componentDidMount(){
+        fetch('/api/skydives')
+        .then(data => data.json())
+        .then(skydives => this.setState({skydives}))
+
+    }
+
    render () {
     return (
         <div>
