162b9b02327365351f142987e82c6853d7210b34
Zelalem Tolera <tolerazelalem@gmail.com>
2018-01-23 16:42:55 -0500

    handle input value and bind the state to SkydiveForm

--- skydives_react_app/src/SkydiveForm.js
+++ skydives_react_app/src/SkydiveForm.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 
 class SkydiveForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {inputValue: 'initial test input ..'}
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange (e) {
+    this.setState({
+        inputValue: e.target.value
+      })
+  }
   render () {
-     return (
-        <div>
-          <input type='text' />
-          <button>Book the Skydive</button>
-        </div>
-      )
-   }
+    return (
+      <div>
+        <input
+          type='text'
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+         />
+        <button>Book the Skydive</button>
+      </div>
+    )
+  }
 }
 export default SkydiveForm
